e5f0fb06829257b3c3ad46119e6836d266413e49
dni1337 <15782082+dni1337@users.noreply.github.com>
2023-04-18 16:09:31 +0200

    Update cam.c

--- sc/PLUGINS/src/cam.c
+++ sc/PLUGINS/src/cam.c
@@ -2846,14 +2846,15 @@ bool cDeCSA::SetDescr(ca_descr_t *ca_descr, bool initial)
       else PRINTF(L_CORE_CSA,"%s.%d: late key set...",devId,idx);
       }
     LDUMP(L_CORE_CSA,ca_descr->cw,8,"%s.%d: %4s key set",devId,idx,ca_descr->parity?"odd":"even");
+    unsigned char ecm = filter->GetECM(0, ca_descr);
     if(ca_descr->parity==0) {
-      set_even_control_word(keys[idx],ca_descr->cw);
+      set_even_control_word(keys[idx],ca_descr->cw,ecm);
       if(!CheckNull(ca_descr->cw,8)) flags[idx]|=FL_EVEN_GOOD|FL_ACTIVITY;
       else PRINTF(L_CORE_CSA,"%s.%d: zero even CW",devId,idx);
       wait.Broadcast();
       }
     else {
-      set_odd_control_word(keys[idx],ca_descr->cw);
+      set_odd_control_word(keys[idx],ca_descr->cw,ecm);
       if(!CheckNull(ca_descr->cw,8)) flags[idx]|=FL_ODD_GOOD|FL_ACTIVITY;
       else PRINTF(L_CORE_CSA,"%s.%d: zero odd CW",devId,idx);
       wait.Broadcast();
