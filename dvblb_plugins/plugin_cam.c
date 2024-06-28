181a6f29cb41d7f235608265135b672a8b66f46a
dni1337 <15782082+dni1337@users.noreply.github.com>
2024-06-28 14:45:38 +0200

    Update plugin_cam.c

--- dvblb_plugins/plugin_cam.c
+++ dvblb_plugins/plugin_cam.c
@@ -16,6 +16,15 @@
 #include "../sc/include/ffdecsawrapper/tools.h"
 #include "../sc/sasccam.h"
 
+struct ca_pid {
+	unsigned int pid;
+	int index;		/* -1 == disable*/
+};
+
+#define CA_SET_PID        _IOW('o', 135, struct ca_pid)
+
+typedef struct ca_pid ca_pid_t;
+
 #define PLUGIN_ID 28
 #define DBG_NAME "CAM"
 
