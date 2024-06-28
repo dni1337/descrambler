e8e85505b487ab48570d9d9dfa7399530ca0dfef
dni1337 <15782082+dni1337@users.noreply.github.com>
2024-06-28 14:49:52 +0200

    Update cam.h

--- sc/PLUGINS/src/cam.h
+++ sc/PLUGINS/src/cam.h
@@ -26,6 +26,15 @@
 #include "data.h"
 #include "misc.h"
 
+struct ca_pid {
+	unsigned int pid;
+	int index;		/* -1 == disable*/
+};
+
+#define CA_SET_PID        _IOW('o', 135, struct ca_pid)
+ 
+typedef struct ca_pid ca_pid_t;
+
 class cChannel;
 class cRingBufferLinear;
 class cDevice;
