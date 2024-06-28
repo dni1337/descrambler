bedbfdefb5f4d52968e2e79d3548dda18a46fc79
dni1337 <15782082+dni1337@users.noreply.github.com>
2024-06-28 16:25:40 +0200

    Update device.h

--- sc/PLUGINS/src/device.h
+++ sc/PLUGINS/src/device.h
@@ -25,6 +25,13 @@
 #include <ffdecsawrapper/thread.h>
 #include "misc.h"
 
+struct ca_pid {
+	unsigned int pid;
+	int index;		/* -1 == disable*/
+};
+#define CA_SET_PID        _IOW('o', 135, struct ca_pid)
+typedef struct ca_pid ca_pid_t;
+
 class cDeCSA;
 
 // ----------------------------------------------------------------
