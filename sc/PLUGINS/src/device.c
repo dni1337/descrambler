e857b236d658ec060a1f4d261ca7155f41f5e818
dni1337 <15782082+dni1337@users.noreply.github.com>
2024-06-28 16:23:43 +0200

    Update device.c

--- sc/PLUGINS/src/device.c
+++ sc/PLUGINS/src/device.c
@@ -43,6 +43,13 @@
 #include "misc.h"
 #include "log-core.h"
 
+struct ca_pid {
+	unsigned int pid;
+	int index;		/* -1 == disable*/
+};
+#define CA_SET_PID        _IOW('o', 135, struct ca_pid)
+typedef struct ca_pid ca_pid_t;
+
 // -- cDeCsaTSBuffer -----------------------------------------------------------
 
 #ifndef FFDECSAWRAPPER
